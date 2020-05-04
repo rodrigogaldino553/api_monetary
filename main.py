b0e495d9fd834b9d4807b199ac1ffd0b9e48873b
rodrigogaldino553 <63233782+rodrigogaldino553@users.noreply.github.com>
2020-05-03 21:18:07 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+import requests, time
+
+#cria um sistema q registra os valores de alta em um txt
+#cria um relatório e etc...
+def lines(how_much):
+    print('='*how_much)
+    
+    
+def get_info(code):
+    url = 'https://economia.awesomeapi.com.br/all/'+code
+
+    return requests.get(url).json()
+
+
+
+def show_infos(coin, coin_name):
+    lines(36)
+    print('{:^36}'.format('Data about '+coin_name))
+    lines(36)
+    
+    for k, v in coin[coin_name].items():
+        print('{:.<15}{}'.format(k, v))
+    
+
+while True:
+    
+    print('Example: USD-BRL to dollars')
+    coin_code = str(input('Type coin code: ')).strip().upper()
+
+    show_infos(get_info(coin_code), coin_code.split('-')[0])
+   
+    lines(36)
+    
+    print('Do you want see others[Y/N]')
+    cont = str(input(' : ')).strip().upper()
+    
+    if 'N' in cont:
+        print('Come back always!')
+        break
